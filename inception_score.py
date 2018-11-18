466b9a4029a112ede3308c85a1754eeac506cee7
Shichang Tang <tangshch@shanghaitech.edu.cn>
2018-11-18 14:50:43 +0800

    Update inception_score.py

--- inception_score.py
+++ inception_score.py
@@ -5,7 +5,8 @@ Code derived from https://github.com/openai/improved-gan/blob/master/inception_s
 Usage:
     Call get_inception_score(images, splits=10)
 Args:
-    images: A numpy array with values ranging from 0 to 255 and shape in the form [N, 3, HEIGHT, WIDTH] where N, HEIGHT and WIDTH can be arbitrary. A dtype of np.uint8 is recommended to save CPU memory.
+    images: A numpy array with values ranging from 0 to 255 and shape in the form [N, 3, HEIGHT, WIDTH] where N, HEIGHT and WIDTH can be arbitrary. 
+            dtype of the images is recommended to be np.uint8 to save CPU memory.
     splits: The number of splits of the images, default is 10.
 Returns:
     Mean and standard deviation of the Inception Score across the splits.
@@ -23,7 +24,7 @@ tfgan = tf.contrib.gan
 session = tf.InteractiveSession()
 
 # A smaller BATCH_SIZE reduces GPU memory usage, but at the cost of a slight slowdown
-BATCH_SIZE=64
+BATCH_SIZE = 64
 
 # Run images through Inception.
 inception_images = tf.placeholder(tf.float32, [BATCH_SIZE, 3, None, None])
