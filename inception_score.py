599bf70e33581f651e60ccb73fd6e3537561172c
Shichang Tang <tangshch@shanghaitech.edu.cn>
2019-08-07 15:25:41 +0800

    Compability with Tensorflow 1.14

--- inception_score.py
+++ inception_score.py
@@ -5,36 +5,43 @@ Code derived from https://github.com/openai/improved-gan/blob/master/inception_s
 Usage:
     Call get_inception_score(images, splits=10)
 Args:
-    images: A numpy array with values ranging from 0 to 255 and shape in the form [N, 3, HEIGHT, WIDTH] where N, HEIGHT and WIDTH can be arbitrary. 
-            dtype of the images is recommended to be np.uint8 to save CPU memory.
+    images: A numpy array with values ranging from 0 to 255 and shape in the form [N, 3, HEIGHT, WIDTH] where N, HEIGHT and WIDTH can be arbitrary. A dtype of np.uint8 is recommended to save CPU memory.
     splits: The number of splits of the images, default is 10.
 Returns:
     Mean and standard deviation of the Inception Score across the splits.
 '''
 
 import tensorflow as tf
-import os, sys
+import os
 import functools
 import numpy as np
 import time
 from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import functional_ops
 tfgan = tf.contrib.gan
 
-session = tf.InteractiveSession()
+session=tf.compat.v1.InteractiveSession()
 
 # A smaller BATCH_SIZE reduces GPU memory usage, but at the cost of a slight slowdown
 BATCH_SIZE = 64
+INCEPTION_URL = 'http://download.tensorflow.org/models/frozen_inception_v1_2015_12_05.tar.gz'
+INCEPTION_FROZEN_GRAPH = 'inceptionv1_for_inception_score.pb'
 
 # Run images through Inception.
-inception_images = tf.placeholder(tf.float32, [BATCH_SIZE, 3, None, None])
+inception_images = tf.compat.v1.placeholder(tf.float32, [None, 3, None, None])
 def inception_logits(images = inception_images, num_splits = 1):
     images = tf.transpose(images, [0, 2, 3, 1])
     size = 299
-    images = tf.image.resize_bilinear(images, [size, size])
+    images = tf.compat.v1.image.resize_bilinear(images, [size, size])
     generated_images_list = array_ops.split(images, num_or_size_splits = num_splits)
-    logits = functional_ops.map_fn(
-        fn = functools.partial(tfgan.eval.run_inception, output_tensor = 'logits:0'),
+    logits = tf.map_fn(
+        fn = functools.partial(
+             tfgan.eval.run_inception, 
+             default_graph_def_fn = functools.partial(
+             tfgan.eval.get_graph_def_from_url_tarball, 
+             INCEPTION_URL, 
+             INCEPTION_FROZEN_GRAPH, 
+             os.path.basename(INCEPTION_URL)), 
+             output_tensor = 'logits:0'),
         elems = array_ops.stack(generated_images_list),
         parallel_iterations = 1,
         back_prop = False,
@@ -46,13 +53,23 @@ def inception_logits(images = inception_images, num_splits = 1):
 logits=inception_logits()
 
 def get_inception_probs(inps):
-    n_batches = len(inps) // BATCH_SIZE
-    preds = np.zeros([n_batches * BATCH_SIZE, 1000], dtype = np.float32)
+    n_batches = int(np.ceil(float(inps.shape[0]) / BATCH_SIZE))
+    preds = np.zeros([inps.shape[0], 1000], dtype = np.float32)
     for i in range(n_batches):
         inp = inps[i * BATCH_SIZE:(i + 1) * BATCH_SIZE] / 255. * 2 - 1
-        preds[i * BATCH_SIZE:(i + 1) * BATCH_SIZE] = logits.eval({inception_images:inp})[:, :1000]
+        preds[i * BATCH_SIZE : i * BATCH_SIZE + min(BATCH_SIZE, inp.shape[0])] = session.run(logits,{inception_images: inp})[:, :1000]
     preds = np.exp(preds) / np.sum(np.exp(preds), 1, keepdims=True)
     return preds
+
+def preds2score(preds, splits=10):
+    scores = []
+    for i in range(splits):
+        part = preds[(i * preds.shape[0] // splits):((i + 1) * preds.shape[0] // splits), :]
+        q = np.expand_dims(np.mean(part, 0), 0)
+        kl = part * (np.log(part / q)) + (1 - part) * np.log((1 - part) / (1 - q))
+        kl = np.mean(kl)
+        scores.append(np.exp(kl))
+    return np.mean(scores), np.std(scores)
 
 def preds2score(preds, splits=10):
     scores = []
@@ -73,4 +90,4 @@ def get_inception_score(images, splits=10):
     preds = get_inception_probs(images)
     mean, std = preds2score(preds, splits)
     print('Inception Score calculation time: %f s' % (time.time() - start_time))
-    return mean, std  # Reference values: 11.34 for 49984 CIFAR-10 training set images, or mean=11.31, std=0.08 if in 10 splits.
+    return mean, std  # Reference values: 11.38 for 50000 CIFAR-10 training set images, or mean=11.31, std=0.10 if in 10 splits.
